3203529fb0bad1bb468dbff31052e07cbf5387af
Guilherme Athayde <guiathayde303@gmail.com>
2021-09-21 23:55:52 -0300

    Permissions Control

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,15 +1,24 @@
-import { destroyCookie } from 'nookies';
 import { useContext } from 'react';
+import { Can } from '../components/Can';
 
 import { AuthContext } from '../contexts/AuthContext';
 import { setupAPIClient } from '../services/api';
-import { AuthTokenError } from '../services/errors/AuthTokenErro';
 import { withSSRAuth } from '../utils/withSSRAuth';
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
-  return <h1>Dashboard {user?.email}</h1>;
+  return (
+    <>
+      <h1>Dashboard {user?.email}</h1>
+
+      <button type="button" onClick={signOut}>SignOut</button>
+
+      <Can permissions={['metrics.list']}>
+        <h2>Métricas</h2>
+      </Can>
+    </>
+  );
 }
 
 export const getServerSideProps = withSSRAuth(async (ctx) => {
